52192ce973ba1e19b984a08333970ef4eded14b0
Kurtis Pritchard <kpritch11@gmail.com>
2018-05-13 09:36:07 -0500

    Initial commit.

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        to: 'kurtis@atmospherelabs.io',
+        from: 'matthew@atmospherelabs.io',
+        text: 'Hey this is Matthew.',
+        createdAt: Date()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+});
